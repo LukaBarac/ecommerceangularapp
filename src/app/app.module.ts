c227f89317d7106466ea3198feb683f2d7cb6840
LukaBarac <lukabarac22@gmail.com>
2023-04-18 14:25:42 +0200

    refaktorisan star rating i dodat angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { RatingModule } from 'ng-starrating';
+import {DhmStarRatingComponent} from 'dhm-star-rating';
 import { SearchComponent } from './search/search.component'
 import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table'; 
 
 @NgModule({
   declarations: [
@@ -27,8 +29,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatingModule,
-    FormsModule
+    DhmStarRatingComponent,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
